4095b81d94a8627ac11b8dd43dab0ef40031f9ad
ahmadsyaifulakbar24 <ipulbelcram@gmail.com>
2022-01-13 12:37:03 +0700

    add new router

--- routes/api.php
+++ routes/api.php
@@ -27,14 +27,18 @@ Route::prefix('v1')->group(function() {
     Route::post('/login', LoginController::class);
     Route::post("/register", RegisterController::class);
 
+    Route::prefix('/param')->group(function() {
+        Route::get('/eselon1', [ParamsController::class, 'get_eselon1']);
+        Route::get('/eselon2', [ParamsController::class, 'get_eselon2']);
+    });
+
     Route::middleware('auth:api')->group(function() {
         Route::get("/user-login", UserController::class);
         Route::delete("/logout", LogoutController::class);
     });
 
-    Route::prefix('/param')->group(function() {
-        Route::get('/eselon1', [ParamsController::class, 'get_eselon1']);
-        Route::get('/eselon2', [ParamsController::class, 'get_eselon2']);
-    });
+    // Route::prefix('/param')->group(function() {
+        
+    // });
 });
 
