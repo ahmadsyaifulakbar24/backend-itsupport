2c4ba86746e2736d6004450683ba9a4d4f881a12
ahmadsyaifulakbar24 <ipulbelcram@gmail.com>
2022-02-23 23:25:20 +0700

    date

--- app/Models/History.php
+++ app/Models/History.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Traits\Uuids;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -19,6 +20,14 @@ class History extends Model
         'history'
     ];
 
+    public function getCreatedAtAttribute($date) {
+        return Carbon::parse($date)->format('Y-m-d H:i:s');
+    }
+
+    public function getUpdatedAtAttribute($date) {
+        return Carbon::parse($date)->format('Y-m-d H:i:s');
+    }
+    
     public function user()
     {
         return $this->belongsTo(User::class, 'action_by');
