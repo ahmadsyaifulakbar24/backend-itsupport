d77ae47a85abf795777e130771486fbccab71b4a
ahmadsyaifulakbar24 <ipulbelcram@gmail.com>
2022-04-06 14:41:12 +0700

    add other status in helpdes resource

--- app/Http/Resources/Helpdesk/HelpdeskDetailResource.php
+++ app/Http/Resources/Helpdesk/HelpdeskDetailResource.php
@@ -32,6 +32,9 @@ class HelpdeskDetailResource extends JsonResource
             'service_category' => $this->service_category,
             'helpdesk_step' => HelpdeskStepResource::collection($this->helpdesk_step()->orderBy('order', 'asc')->get()),
             'helpdesk_step_general_user' => HelpdeskStepResource::collection($this->helpdesk_step()->orderBy('order', 'desc')->limit(1)->get()),
+            'other_status' => [
+                'assigment_status' => ($this->helpdesk_assigment()->count() > 0) ? true : false
+            ],
             'title' => $this->title,
             'email_type' => new ParamResource($this->email_type),
             'email' => EmailNameResource::collection($this->email_name),
